182f2fcb7eb8a324446a58971d37e4d28a95cfaf
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-26 09:59:45 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,24 +1,23 @@
 import React from 'react'
 
-export default class DigitalClicker extends React.component{
-
-  constructor() {
+export default class DigitalClicker extends React.Component{
+  constructor(){
     super();
 
-    this.state ={
-      timesClicked = 0
+    this.state = {
+      timesClicked: 0,
     };
   }
 
   handleEvent = () => {
     this.setState({
-      timesClicked ++ this.state.timesClicked
-    })
+      timesClicked: ++this.state.timesClicked,
+    });
   }
 
-  render() {
+  render(){
     return(
       <button onClick={this.handleEvent}>{this.state.timesClicked}</button>
-    )
+    );
   }
 }
