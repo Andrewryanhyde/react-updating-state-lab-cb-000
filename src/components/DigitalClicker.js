984834af73cdfb66635dc505fce48f7f8f0770a6
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-26 09:58:32 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.component{
+
+  constructor() {
+    super();
+
+    this.state ={
+      timesClicked = 0
+    };
+  }
+  
+  render() {
+    return(
+      
+    )
+  }
+}
